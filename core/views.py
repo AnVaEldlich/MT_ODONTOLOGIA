b3d30dd78370beb39c77182e8f5599e74533d773
Andrius <gironandres.valencia@gmail.com>
2025-11-14 23:43:45 -0500

    Everything perfect

--- core/views.py
+++ core/views.py
@@ -3,28 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    return render(request, 'index.html')
+    return render(request, 'core/index.html')
 
-def about(request):
-    return render(request, 'pages/about.html')
-
-def register(request):
-    return render(request, 'pages/register.html')
-
-def login(request):
-    return render(request, 'pages/TheHome.html')
-
-def registerdoctor(request):
-    return render(request, 'pages/registerdoctor.html')
-
-def pedircita(request):
-    return render(request, 'pages/pedircita.html')
-
-def Tratamientos(request):
-    return render(request, 'pages/Tratamientos.html')
-
-def clinicas(request):
-    return render(request, 'pages/clinicas.html')
-
-def Blog(request):
-    return render(request, 'pages/Blog.html')
+    
